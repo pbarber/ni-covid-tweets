a03c3e9a0b3cf4c1fd3779004ccc0ce8d5ae7ac4
Paul Barber <p.barber@analyticsengines.com>
2021-03-29 09:58:11 +0100

    Support multiple updates

--- sam/r-tweeter/app.py
+++ sam/r-tweeter/app.py
@@ -14,49 +14,51 @@ def lambda_handler(event, context):
     secretobj = sm.get_secret_value(SecretId='ni-covid-tweets')
     secret = json.loads(secretobj['SecretString'])
 
+    messages = []
     # Download the most recently updated PDF file
     s3 = boto3.client('s3')
-    tmp = tempfile.NamedTemporaryFile(suffix='.pdf')
-    with open(tmp.name, 'wb') as fp:
-        s3.download_fileobj(secret['bucketname'],event['keyname'],fp)
-    text = textract.process(tmp.name, method='pdfminer').decode('utf-8')
-    first = True
-    regex = re.compile(r'^Current estimate of Rt \((.*)\):\s+(.*)$')
-    tweet = 'R estimates by Northern Ireland DoH on '
-    for line in text.split('\n'):
-        m = regex.match(line)
-        if first is True:
-            tweet += '%s\n\n' %line
-        elif m:
-            tweet += '\u2022 %s: %s\n' %(m.group(1),m.group(2))
-        first = False
-    tweet += '\nSource: %s' %event['url']
+    for change in event:
+        tmp = tempfile.NamedTemporaryFile(suffix='.pdf')
+        with open(tmp.name, 'wb') as fp:
+            s3.download_fileobj(secret['bucketname'],change['keyname'],fp)
+        text = textract.process(tmp.name, method='pdfminer').decode('utf-8')
+        first = True
+        regex = re.compile(r'^Current estimate of Rt \((.*)\):\s+(.*)$')
+        tweet = 'R estimates by Northern Ireland DoH on '
+        for line in text.split('\n'):
+            m = regex.match(line)
+            if first is True:
+                tweet += '%s\n\n' %line
+            elif m:
+                tweet += '\u2022 %s: %s\n' %(m.group(1),m.group(2))
+            first = False
+        tweet += '\nSource: %s' %change['url']
 
-    if event.get('notweet') is not True:
-        auth = tweepy.OAuthHandler(secret['twitter_apikey'], secret['twitter_apisecretkey'])
-        auth.set_access_token(secret['twitter_accesstoken'], secret['twitter_accesstokensecret'])
+        if change.get('notweet') is not True:
+            auth = tweepy.OAuthHandler(secret['twitter_apikey'], secret['twitter_apisecretkey'])
+            auth.set_access_token(secret['twitter_accesstoken'], secret['twitter_accesstokensecret'])
 
-        api = tweepy.API(auth)
+            api = tweepy.API(auth)
 
-        resp = api.update_status(tweet)
+            resp = api.update_status(tweet)
 
-        # Download and update the index
-        status = S3_scraper_index(s3, secret['bucketname'], secret['doh-r-index'])
-        index = status.get_dict()
-        for i in range(len(index)):
-            if index[i]['filedate'] == event['filedate']:
-                index[i]['tweet'] = resp.id
-                break
-        status.put_dict(index)
+            # Download and update the index
+            status = S3_scraper_index(s3, secret['bucketname'], secret['doh-r-index'])
+            index = status.get_dict()
+            for i in range(len(index)):
+                if index[i]['filedate'] == change['filedate']:
+                    index[i]['tweet'] = resp.id
+                    break
+            status.put_dict(index)
 
-        message = 'Tweeted ID %s and updated %s' %(resp.id, secret['doh-r-index'])
-    else:
-        print(tweet)
-        message = 'Did not tweet'
+            messages.append('Tweeted ID %s and updated %s' %(resp.id, secret['doh-r-index']))
+        else:
+            print(tweet)
+            messages.append('Did not tweet')
 
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": message,
+            "message": messages,
         }),
     }
