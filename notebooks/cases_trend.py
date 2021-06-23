c87fae6f42e43c29045f7d13109ce0d5e457e974
Paul Barber <p.barber@analyticsengines.com>
2021-06-23 10:14:47 +0100

    Started cleaning up code

--- notebooks/cases_trend.py
+++ notebooks/cases_trend.py
@@ -4,12 +4,55 @@ import altair
 import numpy
 import datetime
 
+# %% Exponential fitting functions
+def calc_exp_fit0(data):
+    curve = numpy.polyfit(data.index, numpy.log(data.values), 1)
+    return curve[0]
+
+def calc_exp_fit1(data):
+    curve = numpy.polyfit(data.index, numpy.log(data.values), 1)
+    return curve[1]
+
+def get_model_for_area(df, to_model):
+    df.set_index('x', inplace=True)
+    df['%s model0'%to_model] = df[to_model].rolling(window=9, center=True).apply(calc_exp_fit0)
+    df['%s model1'%to_model] = df[to_model].rolling(window=9, center=True).apply(calc_exp_fit1)
+    return df[['%s model0'%to_model,'%s model1'%to_model]]
+
+def create_models(df, areakey, to_model):
+    df['x'] = (df['Date'] - df['Date'].min()).dt.days
+    df = df.merge(
+        df.groupby(areakey).apply(get_model_for_area, to_model=to_model),
+        left_on=[areakey,'x'],
+        right_index=True,
+        validate='1:1'
+    )
+    df['%s model_daily_change' %to_model] = (fit_exp(df['%s model0'%to_model], df['%s model1'%to_model], 2) - fit_exp(df['%s model0'%to_model], df['%s model1'%to_model], 1)) / fit_exp(df['%s model0'%to_model], df['%s model1'%to_model], 1)
+    df['%s model_weekly_change' %to_model] = (fit_exp(df['%s model0'%to_model], df['%s model1'%to_model], 8) - fit_exp(df['%s model0'%to_model], df['%s model1'%to_model], 1)) / fit_exp(df['%s model0'%to_model], df['%s model1'%to_model], 1)
+    return(df)
+
 # %% 2019 populations from Fig 3 of https://www.ons.gov.uk/peoplepopulationandcommunity/populationandmigration/populationestimates/bulletins/annualmidyearpopulationestimates/mid2019estimates
 nationpop = pandas.DataFrame([
     {'Nation': 'Northern Ireland', 'Population': 1893667},
     {'Nation': 'England', 'Population': 56286961},
     {'Nation': 'Scotland', 'Population': 5463300},
     {'Nation': 'Wales', 'Population': 3152879},
+])
+
+# %% NISRA mid-year LGD population estimates
+nipop = pandas.DataFrame([
+    {"Area": "Antrim and Newtownabbey","Population": 143504},
+    {"Area": "Armagh City, Banbridge and Craigavon","Population": 216205},
+    {"Area": "Belfast","Population": 343542},
+    {"Area": "Causeway Coast and Glens","Population": 144838},
+    {"Area": "Derry City and Strabane","Population": 151284},
+    {"Area": "Fermanagh and Omagh","Population": 117397},
+    {"Area": "Lisburn and Castlereagh","Population": 146002},
+    {"Area": "Mid and East Antrim","Population": 139274},
+    {"Area": "Ards and North Down","Population": 161725},
+    {"Area": "Mid Ulster","Population": 148528},
+    {"Area": "Newry, Mourne and Down","Population": 181368},
+    {"Area": "Missing Postcode","Population": 0}
 ])
 
 # %% Load data from PHE API and transform it to the right format
@@ -24,6 +67,21 @@ df = df.rename(columns={'index': 'Date'}).sort_values('Date')
 df['New cases 7-day rolling mean'] = df.groupby('Nation').rolling(7, center=True).mean().droplevel(0)
 df = df.merge(nationpop, how='left', left_on='Nation', right_on='Nation', validate='m:1')
 df['Rolling cases per 100k'] = 100000 * (df['New cases 7-day rolling mean'] / df['Population'])
+df = create_models(df, 'Nation', 'Rolling cases per 100k')
+df = create_models(df, 'Nation', 'newCasesBySpecimenDate')
+
+# %% Load NI regional data
+ni = pandas.read_excel('~/Downloads/doh-dd-110521.xlsx', sheet_name='Tests')
+ni.rename(columns={'LGD2014NAME': 'Area', 'Date of Specimen': 'Date'}, inplace=True)
+ni['Area'] = ni['Area'].fillna('Missing Postcode')
+newind = pandas.date_range(start=ni['Date'].min(), end=ni['Date'].max())
+ni.fillna(0, inplace=True)
+ni = ni.groupby(['Date','Area']).sum()
+ni = ni.reindex(pandas.MultiIndex.from_product([newind,ni.index.levels[1]], names=['Date','Area']), fill_value=0).reset_index()
+ni['New cases 7-day rolling mean'] = ni.groupby('Area')['Individ with Positive Lab Test'].rolling(7, center=True).mean().droplevel(0)
+ni = ni.merge(nipop, how='left', left_on='Area', right_on='Area', validate='m:1')
+ni['Rolling cases per 100k'] = 100000 * (ni['New cases 7-day rolling mean'] / ni['Population'])
+ni = create_models(ni, 'Area', 'Rolling cases per 100k')
 
 # %%
 covid_timeline = pandas.DataFrame([
@@ -64,7 +122,7 @@ def covid_timeline_base_chart(days=1000):
     )
 
 # %% Trendline functions
-def plot_multiple_trendlines(df, y, color, y_scale=None, domain=None, range=None):
+def plot_multiple_trendlines(df, y, color, y_scale=None, domain=None, range=None, y_format=',d', y_title='Title'):
     if y_scale == 'log':
         y_def = altair.Y(
             field=y,
@@ -72,11 +130,19 @@ def plot_multiple_trendlines(df, y, color, y_scale=None, domain=None, range=None
             scale=altair.Scale(
                 type='log'
             ),
+            axis=altair.Axis(
+                format=y_format,
+                title=y_title,
+            ),
         )
     else:
         y_def = altair.Y(
             field=y,
-            type='quantitative'
+            type='quantitative',
+            axis=altair.Axis(
+                format=y_format,
+                title=y_title,
+            ),
         )
     if domain is not None:
         c_def = altair.Color(
@@ -104,7 +170,16 @@ def plot_multiple_trendlines(df, y, color, y_scale=None, domain=None, range=None
         color=c_def
     ).properties(
         height=450,
-        width=800
+        width=800,
+        title=altair.TitleParams(
+            'https://twitter.com/ni_covid19_data',
+            baseline='bottom',
+            orient='bottom',
+            anchor='end',
+            fontWeight='normal',
+            fontSize=10,
+            dy=10
+        ),
     )
 
 def plot_single_trendline(df, y, color, y_format=',', y_scale=None, x_axis=None, domain=None, range=None):
@@ -357,64 +432,6 @@ plt = plot_single_trendline_fit(df, 'Rolling cases per 100k', 'New cases per 100
 plt.save('eng-%s.png'%datetime.datetime.now().date().strftime('%Y-%d-%m'))
 plt
 
-# %% NISRA mid-year LGD population estimates
-nipop = pandas.DataFrame([
-    {"Area": "Antrim and Newtownabbey","Population": 143504},
-    {"Area": "Armagh City, Banbridge and Craigavon","Population": 216205},
-    {"Area": "Belfast","Population": 343542},
-    {"Area": "Causeway Coast and Glens","Population": 144838},
-    {"Area": "Derry City and Strabane","Population": 151284},
-    {"Area": "Fermanagh and Omagh","Population": 117397},
-    {"Area": "Lisburn and Castlereagh","Population": 146002},
-    {"Area": "Mid and East Antrim","Population": 139274},
-    {"Area": "Ards and North Down","Population": 161725},
-    {"Area": "Mid Ulster","Population": 148528},
-    {"Area": "Newry, Mourne and Down","Population": 181368},
-    {"Area": "Missing Postcode","Population": 0}
-])
-
-# %%
-def calc_exp_fit0(data):
-    curve = numpy.polyfit(data.index, numpy.log(data.values), 1)
-    return curve[0]
-
-def calc_exp_fit1(data):
-    curve = numpy.polyfit(data.index, numpy.log(data.values), 1)
-    return curve[1]
-
-def get_model_for_area(df):
-    df.set_index('x', inplace=True)
-    df['model0'] = df['Rolling cases per 100k'].rolling(window=9, center=True).apply(calc_exp_fit0)
-    df['model1'] = df['Rolling cases per 100k'].rolling(window=9, center=True).apply(calc_exp_fit1)
-    return df[['model0','model1']]
-
-def create_models(df, areakey):
-    df['x'] = (df['Date'] - df['Date'].min()).dt.days
-    df = df.merge(
-        df.groupby(areakey).apply(get_model_for_area),
-        left_on=[areakey,'x'],
-        right_index=True,
-        validate='1:1'
-    )
-    print(df)
-    df['model_daily_change'] = (fit_exp(df['model0'], df['model1'], 2) - fit_exp(df['model0'], df['model1'], 1)) / fit_exp(df['model0'], df['model1'], 1)
-    df['model_weekly_change'] = (fit_exp(df['model0'], df['model1'], 8) - fit_exp(df['model0'], df['model1'], 1)) / fit_exp(df['model0'], df['model1'], 1)
-    return(df)
-
-ni = pandas.read_excel('~/Downloads/doh-dd-110521.xlsx', sheet_name='Tests')
-ni.rename(columns={'LGD2014NAME': 'Area', 'Date of Specimen': 'Date'}, inplace=True)
-ni['Area'] = ni['Area'].fillna('Missing Postcode')
-newind = pandas.date_range(start=ni['Date'].min(), end=ni['Date'].max())
-ni.fillna(0, inplace=True)
-ni = ni.groupby(['Date','Area']).sum()
-ni = ni.reindex(pandas.MultiIndex.from_product([newind,ni.index.levels[1]], names=['Date','Area']), fill_value=0).reset_index()
-ni['New cases 7-day rolling mean'] = ni.groupby('Area')['Individ with Positive Lab Test'].rolling(7, center=True).mean().droplevel(0)
-ni = ni.merge(nipop, how='left', left_on='Area', right_on='Area', validate='m:1')
-ni['Rolling cases per 100k'] = 100000 * (ni['New cases 7-day rolling mean'] / ni['Population'])
-ni = create_models(ni, 'Area')
-df = create_models(df, 'Nation')
-
-
 # %%
 plot_multiple_trendlines(
     ni[ni['Date']>'2020-12-24'],
@@ -475,9 +492,23 @@ plot_single_trendline(
 
 # %%
 plot_multiple_trendlines(
-    ni[ni['Date']>'2020-11-24'],
+    ni[(ni['Date']>'2020-11-24') & (~ni['model_daily_change'].isna())],
     y='model_daily_change',
-    color='Area'
+    color='Area',
+    y_format='%',
+    y_title='Daily change in 7-day rolling average of cases',
+).properties(
+    height=450,
+    width=800
+)
+
+# %%
+plot_multiple_trendlines(
+    ni[(ni['Date']>'2021-04-23') & (~ni['model_daily_change'].isna())],
+    y='model_daily_change',
+    color='Area',
+    y_format='%',
+    y_title='Daily change in 7-day rolling average of cases',
 ).properties(
     height=450,
     width=800
@@ -486,11 +517,89 @@ plot_multiple_trendlines(
 # %%
 plot_multiple_trendlines(
     df[df['Date']>'2020-11-24'],
-    y='model_daily_change',
+    y='Rolling cases per 100k model_daily_change',
     color='Nation',
     domain=['England','Scotland','Wales','Northern Ireland'],
-    range=['grey','#005eb8','#D30731','#076543']
+    range=['grey','#005eb8','#D30731','#076543'],
+    y_format='%',
+    y_title='Daily change in 7-day rolling average of cases',
 ).properties(
+    height=450,
+    width=800
+)
+
+# %%
+plt = plot_multiple_trendlines(
+    df[(df['Date']>'2021-04-23') & (~df['Rolling cases per 100k model_daily_change'].isna())],
+    y='Rolling cases per 100k model_daily_change',
+    color='Nation',
+    domain=['England','Scotland','Wales','Northern Ireland'],
+    range=['grey','#005eb8','#D30731','#076543'],
+    y_format='%',
+    y_title='Daily change in 7-day rolling average of cases',
+).properties(
+    height=450,
+    width=800
+)
+plt.save('nations-daily-change-%s.png'%datetime.datetime.now().date().strftime('%Y-%d-%m'))
+plt
+
+# %%
+def plot_points_average_and_trend(df, colour):
+    df1 = df[(~df['newCasesBySpecimenDate'].isna()) & (df['newCasesBySpecimenDate'] != 0)]
+    df2 = df[(~df['New cases 7-day rolling mean'].isna()) & (df['New cases 7-day rolling mean'] != 0)]
+    return altair.layer(
+        altair.Chart(
+            df1
+        ).mark_point(
+            color=colour,
+            opacity=0.7,
+            filled=True,
+            size=15,
+        ).encode(
+            x=altair.X(
+                field='Date',
+                type='temporal'
+            ),
+            y=altair.Y(
+                field='newCasesBySpecimenDate',
+                type='quantitative',
+                aggregate='sum',
+                scale=altair.Scale(
+                    type='log'
+                ),
+            )
+        ),
+        altair.Chart(
+            df2
+        ).mark_line(
+            color=colour
+        ).encode(
+            x=altair.X(
+                field='Date',
+                type='temporal'
+            ),
+            y=altair.Y(
+                field='New cases 7-day rolling mean',
+                type='quantitative',
+                aggregate='sum',
+                scale=altair.Scale(
+                    type='log'
+                ),
+            )
+        ),
+    ).properties(
+        title=altair.TitleParams(
+            'https://twitter.com/ni_covid19_data',
+            baseline='bottom',
+            orient='bottom',
+            anchor='end',
+            fontWeight='normal',
+            fontSize=10,
+            dy=10
+        ),
+    )
+plot_points_average_and_trend(df[(df['Nation']=='England') & (df['Date'] > '2021-04-23')],'grey').properties(
     height=450,
     width=800
 )
