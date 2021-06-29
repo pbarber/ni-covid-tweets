1412ce367cb058c9d82facaf1293841544e804a3
Paul Barber <p.barber@analyticsengines.com>
2021-06-29 20:45:35 +0100

    Proper variant plotting

--- notebooks/cog_variants.py
+++ notebooks/cog_variants.py
@@ -1,42 +1,94 @@
 #%%
 import io
+import datetime
 
 import pandas
 import altair
 
 #%%
 #df = pandas.read_csv('../data/2021-05-18T16_29_53-116060678.csv')
-df = pandas.read_csv('../data/2021-05-24T10_24_59-55748702.csv')
+df = pandas.read_csv('../data/2021-05-18T16_29_53-116060678.csv')
 df = df[df['adm1']=='UK-NIR']
 
 #%%
-lin_by_day = df.groupby(['sample_date','lineage']).size().reset_index(name='count')
-lin_by_week = df.groupby(['epi_week','lineage']).size().reset_index(name='count')
-lineage = df.groupby('lineage').size().reset_index(name='count')
-lin_by_area = df.groupby(['iso_3166_code','lineage']).size().reset_index(name='count')
+lineage_lookup = pandas.DataFrame([
+    {'WHO label': 'Alpha', 'Pango lineage': 'B.1.1.7'},
+    {'WHO label': 'Beta', 'Pango lineage': 'B.1.351'},
+    {'WHO label': 'Gamma', 'Pango lineage': 'P.1'},
+    {'WHO label': 'Delta', 'Pango lineage': 'B.1.617.2'},
+    {'WHO label': 'Epsilon', 'Pango lineage': 'B.1.427'},
+    {'WHO label': 'Zeta', 'Pango lineage': 'P.2'},
+    {'WHO label': 'Eta', 'Pango lineage': 'B.1.525'},
+    {'WHO label': 'Theta', 'Pango lineage': 'P.3'},
+    {'WHO label': 'Iota', 'Pango lineage': 'B.1.526'},
+    {'WHO label': 'Kappa', 'Pango lineage': 'B.1.617.1'},
+    {'WHO label': 'Lambda', 'Pango lineage': 'C.37'},
+])
+
+#%%
+df['Sample Date'] = pandas.to_datetime(df['sample_date'])
+df['Week of sample'] = df['Sample Date'] - pandas.to_timedelta(df['Sample Date'].dt.dayofweek, unit='d')
+df = df.merge(lineage_lookup, how='left', left_on='lineage', right_on='Pango lineage')
+df['WHO label'] = df['WHO label'].fillna('Other')
+lin_by_day = df.groupby(['Sample Date','WHO label']).size().reset_index(name='count')
+lin_by_week = df.groupby(['Week of sample','WHO label']).size().rename('count')
+lin_pc_by_week = lin_by_week/lin_by_week.groupby(level=0).sum()
+lin_by_week = pandas.DataFrame(lin_by_week).reset_index()
+lin_pc_by_week = pandas.DataFrame(lin_pc_by_week).reset_index()
+lineage = df.groupby('WHO label').size().reset_index(name='count')
+lin_by_area = df.groupby(['iso_3166_code','WHO label']).size().reset_index(name='count')
 
 #%%
 altair.Chart(
-    lin_by_day[lin_by_day['sample_date'] > '2021-03-31']
+    lin_by_day[lin_by_day['Sample Date'] > df['Sample Date'].max()-pandas.to_timedelta(84, unit='d')]
 ).mark_area().encode(
-    x = 'sample_date:T',
+    x = 'Sample Date:T',
     y = 'count:Q',
-    color='lineage'
+    color='WHO label'
 )
 
 #%%
-altair.Chart(
-    lin_by_week[lin_by_week['epi_week']> 67]
-).mark_area().encode(
-    x = 'epi_week:O',
-    y = 'count:Q',
-    color='lineage'
+p = altair.vconcat(
+    altair.Chart(
+        lin_by_week[lin_by_week['Week of sample']>lin_by_week['Week of sample'].max()-pandas.to_timedelta(84, unit='d')]
+    ).mark_line().encode(
+        x = altair.X('Week of sample:T', axis=altair.Axis(title='', labels=False, ticks=False)),
+        y = altair.Y('count:Q', axis=altair.Axis(title='Samples')),
+        color='WHO label'
+    ).properties(
+        height=225,
+        width=800,
+        title='NI COVID-19 variants identified by COG-UK over the most recent 12 weeks'
+    ),
+    altair.Chart(
+        lin_pc_by_week[lin_pc_by_week['Week of sample']>lin_pc_by_week['Week of sample'].max()-pandas.to_timedelta(84, unit='d')]
+    ).mark_area().encode(
+        x = 'Week of sample:T',
+        y = altair.Y('sum(count):Q', axis=altair.Axis(format='%', title='% of samples')),
+        color='WHO label'
+    ).properties(
+        height=225,
+        width=800,
+    )
+).properties(
+    title=altair.TitleParams(
+        ['Variant identification can take up to 3 weeks, so recent totals are likely to be revised upwards',
+        'https://twitter.com/ni_covid19_data on %s'  %datetime.datetime.now().date().strftime('%A %-d %B %Y')],
+        baseline='bottom',
+        orient='bottom',
+        anchor='end',
+        fontWeight='normal',
+        fontSize=10,
+        dy=10
+    ),
 )
+p.save('ni-variants-%s.png'%datetime.datetime.now().date().strftime('%Y-%d-%m'))
+p
 
 #%%
-lin_by_area[lin_by_area['lineage']=='B.1.617.2']
+lin_by_area[lin_by_area['WHO label']=='Delta']
 # %%
-lineage[lineage['lineage'].str.strip()=='B.1.617.2']
+lineage[lineage['WHO label'].str.strip()=='Delta']
 # %%
 len(df)
 # %%
