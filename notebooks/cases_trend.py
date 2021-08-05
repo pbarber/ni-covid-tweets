0d65bcc2942ddc9c2e7bf96cf1a2bead6edc76cf
Paul Barber <p.barber@analyticsengines.com>
2021-08-05 22:04:18 +0100

    Shared plotting code

--- notebooks/cases_trend.py
+++ notebooks/cases_trend.py
@@ -4,6 +4,7 @@ import altair
 import numpy
 import datetime
 from sklearn.linear_model import LinearRegression
+from plot_shared import plot_key_ni_stats_date_range, plot_points_average_and_trend
 
 # %% Exponential fitting functions
 def calc_exp_fit0(data):
@@ -572,150 +573,6 @@ plt = plot_multiple_trendlines(
 plt.save('nations-daily-change-%s.png'%datetime.datetime.now().date().strftime('%Y-%d-%m'))
 plt
 
-def points_average_and_trend(points, line, colour, date_col, x_title, y_title, scale='linear', width=800, height=450, x_type='temporal', colour_domain=[], colour_range=[]):
-    if scale=='log':
-        y_title += ' (log scale)'
-        line_df = line[(~line.isna()) & (line != 0)].reset_index(name='line')
-    else:
-        line_df = line[~line.isna()].reset_index(name='line')
-    encode_point_args = {
-        'x': altair.X(
-            field=date_col,
-            type=x_type,
-            axis=altair.Axis(title=x_title),
-        ),
-        'y': altair.Y(
-            field='points',
-            type='quantitative',
-            aggregate='sum',
-            axis=altair.Axis(title=''),
-            scale=altair.Scale(
-                type=scale
-            ),
-        ),
-    }
-    encode_line_args = {
-        'x': altair.X(
-            field=date_col,
-            type=x_type
-        ),
-        'y': altair.Y(
-            field='line',
-            type='quantitative',
-            aggregate='sum',
-            scale=altair.Scale(
-                type=scale
-            ),
-            axis=altair.Axis(title=y_title),
-        ),
-    }
-    mark_point_args = {
-        'opacity':0.7,
-        'filled':True,
-        'size':15,
-    }
-    mark_line_args = {
-    }
-    if colour in line_df.columns:
-        if len(colour_domain) == 0:
-            encode_point_args['color'] = colour
-            encode_line_args['color'] = colour
-        else:
-            encode_point_args['color'] = altair.Color(
-                field=colour,
-                type='nominal',
-                scale=altair.Scale(
-                    domain=colour_domain,
-                    range=colour_range
-                )
-            )
-            encode_line_args['color'] = encode_point_args['color']
-    else:
-        mark_point_args['color'] = colour
-        mark_line_args['color'] = colour
-    charts = [
-        altair.Chart(
-            line_df
-        ).mark_line(
-            **mark_line_args
-        ).encode(
-            **encode_line_args
-        ).properties(
-            width=width,
-            height=height
-        ),
-    ]
-    if points is not None:
-        if scale=='log':
-            points_df = points[(~points.isna()) & (points != 0)].reset_index(name='points')
-        else:
-            points_df = points[~points.isna()].reset_index(name='points')
-        charts.append(
-            altair.Chart(
-                points_df
-            ).mark_point(
-                **mark_point_args
-            ).encode(
-                **encode_point_args
-            )
-        )
-    return altair.layer(*charts
-    )
-
-def plot_points_average_and_trend(configs, title, footer):
-    return altair.concat(
-        altair.vconcat(
-            *[points_average_and_trend(
-                **c
-            ) for c in configs]
-        ).resolve_scale(
-            x='shared'
-        ).properties(
-            title=altair.TitleParams(
-                footer,
-                baseline='bottom',
-                orient='bottom',
-                anchor='end',
-                fontWeight='normal',
-                fontSize=10,
-                dy=10
-            ),
-        )
-    ).properties(
-        title=altair.TitleParams(
-            title,
-            anchor='middle',
-        )
-    )
-
-# %%
-plt = plot_points_average_and_trend(
-    [
-        {
-            'points': None,
-            'line': df[(df['Date'] > '2021-05-08')].set_index(['Date','Nation'])['Rolling cases per 100k'],
-            'colour': 'Nation',
-            'date_col': 'Date',
-            'x_title': 'Specimen Date',
-            'y_title': 'New cases per 100k',
-            'scale': 'log',
-            'colour_domain': ['England','Scotland','Wales','Northern Ireland'],
-            'colour_range': ['grey','#005eb8','#D30731','#076543']
-        },
-    ],
-    '%s COVID-19 %s (7-day average, log scale) reported on %s' %(
-        'UK',
-        'cases per 100k people',
-        datetime.datetime.today().strftime('%A %-d %B %Y'),
-    ),
-    [
-        'Cases data from PHE dashboard/API, mid-2020 populations from ONS',
-        'https://twitter.com/ni_covid19_data'
-    ]
-)
-plt.save('nations-cases-100k-log-%s.png'%datetime.datetime.now().date().strftime('%Y-%d-%m'))
-plt
-
 # %%
 for scale in ['log','linear']:
     plt = plot_points_average_and_trend(
@@ -744,109 +601,22 @@ for scale in ['log','linear']:
         ]
     )
     plt.save('nations-cases-100k-%s-%s.png'%(scale,datetime.datetime.now().date().strftime('%Y-%d-%m')))
-    plt
-
-# %%
-plot_points_average_and_trend(
-    [
-        {
-            'points': df[(df['Nation']=='Northern Ireland') & (df['Date'] > '2021-06-01')].set_index('Date')['newCasesBySpecimenDate'],
-            'line': df[(df['Nation']=='Northern Ireland') & (df['Date'] > '2021-06-01')].set_index('Date')['New cases 7-day rolling mean'],
-            'colour': '#076543',
-            'date_col': 'Date',
-            'x_title': 'Specimen Date',
-            'y_title': 'New cases',
-            'scale': 'linear',
-            'height': 225
-        },
-        {
-            'points': admissions[(admissions['Admission Date'] > '2021-06-01')].set_index('Admission Date')['Number of Admissions'],
-            'line': admissions[(admissions['Admission Date'] > '2021-06-01')].set_index('Admission Date')['Number of Admissions 7-day rolling mean'],
-            'colour': '#076543',
-            'date_col': 'Admission Date',
-            'x_title': 'Date',
-            'y_title': 'Hospital admissions',
-            'scale': 'linear',
-            'height': 225
-        },
-    ],
-    '%s COVID-19 %s (daily and 7-day mean) reported on %s' %(
-        'Northern Ireland',
-        'cases and admissions',
-        datetime.datetime.today().strftime('%A %-d %B %Y'),
-    ),
-    [
-        'Dots show daily reports, line is 7-day rolling average',
-        'Cases, admissions and deaths data from DoH daily data',
-        'https://twitter.com/ni_covid19_data'
-    ],
-)
-
-# %%
-def plot_key_ni_stats_date_range(df, admissions, deaths, start_date, end_date):
-    return plot_points_average_and_trend(
-        [
-            {
-                'points': df[(df['Nation']=='Northern Ireland') & (df['Date'] >= start_date) & (df['Date'] <= end_date)].set_index('Date')['newCasesBySpecimenDate'],
-                'line': df[(df['Nation']=='Northern Ireland') & (df['Date'] >= start_date) & (df['Date'] <= end_date)].set_index('Date')['New cases 7-day rolling mean'],
-                'colour': '#076543',
-                'date_col': 'Date',
-                'x_title': 'Specimen Date',
-                'y_title': 'New cases',
-                'scale': 'linear',
-                'height': 225
-            },
-            {
-                'points': admissions[(admissions['Admission Date'] >= start_date) & (admissions['Admission Date'] <= end_date)].set_index('Admission Date')['Number of Admissions'],
-                'line': admissions[(admissions['Admission Date'] >= start_date) & (admissions['Admission Date'] <= end_date)].set_index('Admission Date')['Number of Admissions 7-day rolling mean'],
-                'colour': '#076543',
-                'date_col': 'Admission Date',
-                'x_title': 'Date of Admission',
-                'y_title': 'Hospital admissions',
-                'scale': 'linear',
-                'height': 225
-            },
-            {
-                'points': deaths[(deaths['Date of Death'] >= start_date) & (deaths['Date of Death'] <= end_date)].set_index('Date of Death')['Number of Deaths'],
-                'line': deaths[(deaths['Date of Death'] >= start_date) & (deaths['Date of Death'] <= end_date)].set_index('Date of Death')['Number of Deaths 7-day rolling mean'],
-                'colour': '#076543',
-                'date_col': 'Date of Death',
-                'x_title': 'Date of Death',
-                'y_title': 'Deaths within 28 days of positive test',
-                'scale': 'linear',
-                'height': 225
-            },
-        ],
-        '%s COVID-19 %s (daily and 7-day mean) between %s and %s' %(
-            'Northern Ireland',
-            'cases and admissions',
-            datetime.datetime.strptime(start_date, '%Y-%m-%d').strftime('%-d %B %Y'),
-            datetime.datetime.strptime(end_date, '%Y-%m-%d').strftime('%-d %B %Y'),
-        ),
-        [
-            'Dots show daily reports, line is 7-day rolling average',
-            'Cases, admissions and deaths data from DoH daily data',
-            'https://twitter.com/ni_covid19_data'
-        ],
-    )
 
 # %% All data
-# Note how the cases peak is behind the admissions peak as testing ramps up
-# The deaths peak is around 21 days after the admissions peak
-plot_key_ni_stats_date_range(df, admissions, deaths, '2020-03-18', '2021-07-23')
+plot_key_ni_stats_date_range(ni.rename(columns={'Date': 'Sample_Date', 'Individ with Positive Lab Test': 'INDIVIDUALS TESTED POSITIVE'}), admissions, deaths, datetime.datetime.strptime('2020-03-18','%Y-%m-%d'), datetime.datetime.strptime('2021-07-23','%Y-%m-%d'), 'linear')
 
 # %% First wave
 # Note how the cases peak is behind the admissions peak as testing ramps up
 # The deaths peak is around 21 days after the admissions peak
-plot_key_ni_stats_date_range(df, admissions, deaths, '2020-03-18', '2020-05-31')
+plot_key_ni_stats_date_range(ni.rename(columns={'Date': 'Sample_Date', 'Individ with Positive Lab Test': 'INDIVIDUALS TESTED POSITIVE'}), admissions, deaths, datetime.datetime.strptime('2020-03-18','%Y-%m-%d'), datetime.datetime.strptime('2020-05-31','%Y-%m-%d'), 'linear')
 
 # %% Winter 2020 peak
 # Admissions peak is 10 days after the cases peak
 # Deaths peak is 9 days after the admissions peak
-plot_key_ni_stats_date_range(df, admissions, deaths, '2020-12-01', '2021-03-01')
+plot_key_ni_stats_date_range(ni.rename(columns={'Date': 'Sample_Date', 'Individ with Positive Lab Test': 'INDIVIDUALS TESTED POSITIVE'}), admissions, deaths, datetime.datetime.strptime('2020-12-01','%Y-%m-%d'), datetime.datetime.strptime('2021-03-01','%Y-%m-%d'), 'linear')
 
 # %% Summer 2021 growth
-plot_key_ni_stats_date_range(df, admissions, deaths, '2021-06-01', '2021-07-22')
+plot_key_ni_stats_date_range(ni.rename(columns={'Date': 'Sample_Date', 'Individ with Positive Lab Test': 'INDIVIDUALS TESTED POSITIVE'}), admissions, deaths, datetime.datetime.strptime('2021-06-01','%Y-%m-%d'), datetime.datetime.strptime('2021-07-23','%Y-%m-%d'), 'linear')
 
 # %%
 waves = pandas.DataFrame()
@@ -920,7 +690,12 @@ plot_points_average_and_trend(
     '%s COVID-19 %s (daily and 7-day mean) in each phase of the pandemic' %(
         'Northern Ireland',
         'cases, admissions and deaths',
-    )
+    ),
+    [
+        'Dots show daily reports, line is 7-day rolling average',
+        'Cases, admissions and deaths data from DoH daily data',
+        'https://twitter.com/ni_covid19_data'
+    ],
 )
 
 # %% Build dataframe for predicting admissions from cases
--- sam/plot_shared/__init__.py
+++ sam/plot_shared/__init__.py
@@ -2,6 +2,7 @@ import logging
 import os
 
 from selenium import webdriver
+import altair
 
 def get_chrome_driver():
     options = webdriver.ChromeOptions()
@@ -35,3 +36,166 @@ def get_chrome_driver():
     else:
         logging.error('Failed to set up webdriver after %d attempts' %(attempt+1))
     return driver
+
+def points_average_and_trend(points, line, colour, date_col, x_title, y_title, scale='linear', width=800, height=450, x_type='temporal', colour_domain=[], colour_range=[]):
+    if scale=='log':
+        y_title += ' (log scale)'
+        line_df = line[(~line.isna()) & (line != 0)].reset_index(name='line')
+    else:
+        line_df = line[~line.isna()].reset_index(name='line')
+    encode_point_args = {
+        'x': altair.X(
+            field=date_col,
+            type=x_type,
+            axis=altair.Axis(title=x_title),
+        ),
+        'y': altair.Y(
+            field='points',
+            type='quantitative',
+            aggregate='sum',
+            axis=altair.Axis(title=''),
+            scale=altair.Scale(
+                type=scale
+            ),
+        ),
+    }
+    encode_line_args = {
+        'x': altair.X(
+            field=date_col,
+            type=x_type
+        ),
+        'y': altair.Y(
+            field='line',
+            type='quantitative',
+            aggregate='sum',
+            scale=altair.Scale(
+                type=scale
+            ),
+            axis=altair.Axis(title=y_title),
+        ),
+    }
+    mark_point_args = {
+        'opacity':0.7,
+        'filled':True,
+        'size':15,
+    }
+    mark_line_args = {
+    }
+    if colour in line_df.columns:
+        if len(colour_domain) == 0:
+            encode_point_args['color'] = colour
+            encode_line_args['color'] = colour
+        else:
+            encode_point_args['color'] = altair.Color(
+                field=colour,
+                type='nominal',
+                scale=altair.Scale(
+                    domain=colour_domain,
+                    range=colour_range
+                )
+            )
+            encode_line_args['color'] = encode_point_args['color']
+    else:
+        mark_point_args['color'] = colour
+        mark_line_args['color'] = colour
+    charts = [
+        altair.Chart(
+            line_df
+        ).mark_line(
+            **mark_line_args
+        ).encode(
+            **encode_line_args
+        ).properties(
+            width=width,
+            height=height
+        ),
+    ]
+    if points is not None:
+        if scale=='log':
+            points_df = points[(~points.isna()) & (points != 0)].reset_index(name='points')
+        else:
+            points_df = points[~points.isna()].reset_index(name='points')
+        charts.append(
+            altair.Chart(
+                points_df
+            ).mark_point(
+                **mark_point_args
+            ).encode(
+                **encode_point_args
+            )
+        )
+    return altair.layer(*charts
+    )
+
+def plot_points_average_and_trend(configs, title, footer):
+    return altair.concat(
+        altair.vconcat(
+            *[points_average_and_trend(
+                **c
+            ) for c in configs]
+        ).resolve_scale(
+            x='shared'
+        ).properties(
+            title=altair.TitleParams(
+                footer,
+                baseline='bottom',
+                orient='bottom',
+                anchor='end',
+                fontWeight='normal',
+                fontSize=10,
+                dy=10
+            ),
+        )
+    ).properties(
+        title=altair.TitleParams(
+            title,
+            anchor='middle',
+        )
+    )
+
+def plot_key_ni_stats_date_range(df, admissions, deaths, start_date, end_date, scale):
+    return plot_points_average_and_trend(
+        [
+            {
+                'points': df[(df['Sample_Date'] >= start_date) & (df['Sample_Date'] <= end_date)].set_index('Sample_Date')['INDIVIDUALS TESTED POSITIVE'],
+                'line': df[(df['Sample_Date'] >= start_date) & (df['Sample_Date'] <= end_date)].set_index('Sample_Date')['New cases 7-day rolling mean'],
+                'colour': '#076543',
+                'date_col': 'Sample_Date',
+                'x_title': 'Specimen Date',
+                'y_title': 'New cases',
+                'scale': scale,
+                'height': 225
+            },
+            {
+                'points': admissions[(admissions['Admission Date'] >= start_date) & (admissions['Admission Date'] <= end_date)].set_index('Admission Date')['Number of Admissions'],
+                'line': admissions[(admissions['Admission Date'] >= start_date) & (admissions['Admission Date'] <= end_date)].set_index('Admission Date')['Number of Admissions 7-day rolling mean'],
+                'colour': '#076543',
+                'date_col': 'Admission Date',
+                'x_title': 'Date of Admission',
+                'y_title': 'Hospital admissions',
+                'scale': scale,
+                'height': 225
+            },
+            {
+                'points': deaths[(deaths['Date of Death'] >= start_date) & (deaths['Date of Death'] <= end_date)].set_index('Date of Death')['Number of Deaths'],
+                'line': deaths[(deaths['Date of Death'] >= start_date) & (deaths['Date of Death'] <= end_date)].set_index('Date of Death')['Number of Deaths 7-day rolling mean'],
+                'colour': '#076543',
+                'date_col': 'Date of Death',
+                'x_title': 'Date of Death',
+                'y_title': 'Deaths within 28 days of positive test',
+                'scale': scale,
+                'height': 225
+            },
+        ],
+        '%s COVID-19 %s between %s and %s' %(
+            'NI',
+            'cases, admissions and deaths',
+            start_date.strftime('%-d %B %Y'),
+            end_date.strftime('%-d %B %Y'),
+        ),
+        [
+            'Dots show daily reports, line is 7-day rolling average',
+            'Cases, admissions and deaths data from DoH daily data',
+            'https://twitter.com/ni_covid19_data'
+        ],
+    )
