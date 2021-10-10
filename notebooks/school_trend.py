2d22fc51d5f17697d8dc6f66b3fc66d7bf69fb9a
Paul Barber <p.barber@analyticsengines.com>
2021-10-10 20:31:12 +0100

    Added most recent data labels

--- notebooks/school_trend.py
+++ notebooks/school_trend.py
@@ -2,7 +2,7 @@
 import pandas
 import altair
 from plot_shared import plot_points_average_and_trend
-
+import datetime
 
 # %%
 def load_grouped_time_series(df, date_col, group_col, series_col, new_name, model=True):
@@ -38,7 +38,7 @@ case_band_mapping = pandas.DataFrame({
     })
 
 # %%
-admissions = pandas.read_excel('https://www.health-ni.gov.uk/sites/default/files/publications/health/doh-dd-240921.xlsx', sheet_name='Admissions')
+admissions = pandas.read_excel('https://www.health-ni.gov.uk/sites/default/files/publications/health/doh-dd-081021.xlsx', sheet_name='Admissions')
 admissions = admissions.groupby(['Admission Date', 'Age Band'])['Number of Admissions'].sum().reset_index()
 admissions['Admission Date'] = pandas.to_datetime(admissions['Admission Date'])
 admissions = load_grouped_time_series(admissions, 'Admission Date', 'Age Band', 'Number of Admissions', 'Admissions', False)
@@ -60,6 +60,97 @@ altair.Chart(admissions[admissions['Date'] > '2021-07-01']).mark_line().encode(
 # %%
 cases = pandas.read_csv('agebands.csv')
 cases = cases.merge(case_band_mapping, how='left', on='Age_Band_5yr')
+overlay = cases[cases['Date'] == cases['Date'].max()]
+overlay['Nearest'] = ((overlay['Positive_Tests']/overlay['Positive_Tests'].max()) * 40).astype(int) * (overlay['Positive_Tests'].max() / 40)
+#overlay['Nearest'] = overlay['Nearest'].where(overlay['Band Start'] == 5)
+overlay.loc[overlay['Band Start'] == 0, 'Nearest'] = 220
+overlay.loc[overlay['Band Start'] == 5, 'Nearest'] = 900
+overlay.loc[overlay['Band Start'] == 20, 'Nearest'] = 300
+overlay.loc[overlay['Band Start'] == 25, 'Nearest'] = 380
+overlay.loc[overlay['Band Start'] == 30, 'Nearest'] = 460
+overlay.loc[overlay['Band Start'] == 40, 'Nearest'] = 680
+overlay.loc[overlay['Band Start'] == 50, 'Nearest'] = 420
+overlay.loc[overlay['Band Start'] == 55, 'Nearest'] = 340
+overlay.loc[overlay['Band Start'] == 60, 'Nearest'] = 260
+overlay.loc[overlay['Band Start'] == 65, 'Nearest'] = 90
+overlay.loc[overlay['Band Start'] == 75, 'Nearest'] = 50
+
+trend = altair.Chart(cases).mark_line().encode(
+    x = 'Date:T',
+    y = altair.Y(
+        field='Positive_Tests',
+        type='quantitative',
+        aggregate='sum',
+        axis=altair.Axis(title='Cases per day (7 day total)'),
+    ),
+    color = altair.Color(
+        'Age_Band_5yr:N',
+        sort=altair.EncodingSortField('Band Start'),
+        legend=None
+    ),
+)
+
+text = altair.Chart(overlay).mark_text(
+        align='left',
+        baseline='middle',
+        dx=5
+    ).encode(
+        x = 'Date:T',
+        y = altair.Y(
+            field='Nearest',
+            type='quantitative',
+            aggregate='sum',
+            axis=altair.Axis(title='Cases per day (7 day total)'),
+        ),
+        color = altair.Color(
+            'Age_Band_5yr:N',
+            sort=altair.EncodingSortField('Band Start'),
+            legend=None
+        ),
+        text = altair.Text('Age_Band_5yr')
+    )
+
+plt = altair.concat(
+    altair.layer(
+        trend,
+        text
+    ).properties(
+        height=450,
+        width=800,
+        title=altair.TitleParams(
+            [
+                'From DoH daily data',
+                'https://twitter.com/ni_covid19_data on %s'  %datetime.datetime.now().date().strftime('%A %-d %B %Y'),
+            ],
+            baseline='bottom',
+            orient='bottom',
+            anchor='end',
+            fontWeight='normal',
+            fontSize=10,
+            dy=10
+        ),
+    )
+).properties(
+    title=altair.TitleParams(
+        'NI COVID-19 cases by age band',
+        anchor='middle',
+    )
+)
+plt.save('ni-age-band-cases-%s.png'%(datetime.datetime.now().date().strftime('%Y-%m-%d')))
+plt
+
+# %%
+cases_total = cases.groupby(['Date','Age_Band_5yr'])['Positive_Tests'].sum()
+cases_pc = cases_total.div(cases.groupby('Date')['Positive_Tests'].sum(),level='Date').reset_index()
+
+altair.Chart(
+    cases_pc
+).mark_area().encode(
+    x = 'Date:T',
+    y = altair.Y('Positive_Tests:Q', axis=altair.Axis(format='%', title='% of cases', orient="right")),
+    color='Age_Band_5yr'
+)
+
 
 # %%
 altair.Chart(cases[cases['Date'] > '2021-07-01']).mark_line().encode(
@@ -82,11 +173,17 @@ altair.Chart(cases).mark_line().encode(
         aggregate='sum',
         axis=altair.Axis(title='Cases per day (7 day total)'),
     ),
-    facet = altair.Facet('Age_Band_5yr:N', columns=4, sort=altair.EncodingSortField('Band Start')),
+    facet = altair.Facet(
+        'Age_Band_5yr',
+        columns=6
+    )
 ).properties(
     height=100,
     width=100
 )
+
+# %%
+
 
 # %%
 plt = plot_points_average_and_trend(
