7d5917429263eb794d5d79fb979b58eb96dd6abf
Paul Barber <p.barber@analyticsengines.com>
2022-01-26 19:46:35 +0000

    Improved vaccine gap analysis

--- notebooks/vacc_booster_gap.py
+++ notebooks/vacc_booster_gap.py
@@ -2,34 +2,27 @@
 import datetime
 
 import pandas
-import altair
 from plot_shared import plot_points_average_and_trend
 
 # %%
-df = pandas.read_csv('https://api.coronavirus.data.gov.uk/v2/data?areaType=nation&metric=cumPeopleVaccinatedFirstDoseByPublishDate&metric=cumPeopleVaccinatedSecondDoseByPublishDate&format=csv')
+df = pandas.read_csv('https://api.coronavirus.data.gov.uk/v2/data?areaType=nation&metric=cumPeopleVaccinatedFirstDoseByPublishDate&metric=cumPeopleVaccinatedThirdInjectionByPublishDate&metric=cumPeopleVaccinatedSecondDoseByPublishDate&format=csv')
 df.rename(columns={
     'cumPeopleVaccinatedFirstDoseByPublishDate': 'First',
     'cumPeopleVaccinatedSecondDoseByPublishDate': 'Second',
+    'cumPeopleVaccinatedThirdInjectionByPublishDate': 'Third/Booster',
     'areaName': 'Nation',
     'date': 'Publication Date'
 }, inplace=True)
 df = df.drop(columns=['areaCode','areaType']).melt(id_vars=['Publication Date','Nation'], var_name='Dose', value_name='People')
+all = df[df['Nation']=='Northern Ireland'][['Publication Date','Dose','People']]
+#ni.rename(columns={'Date':'Publication Date','Total':'People'}, inplace=True)
+#all = ni
+#all = history.merge(ni, on=['Publication Date','Dose'], how='outer', suffixes=('','_bot'))
+#all['People'] = all['People'].fillna(all['People_bot'])
+#all = all[['Publication Date','Dose','People']]
 
 # %%
-ni = pandas.read_csv('../sam/doses.csv')
-ni['Dose'] = ni['Dose'].str.replace('Dose 1', 'First')
-ni['Dose'] = ni['Dose'].str.replace('Dose 2', 'Second')
-ni['Dose'] = ni['Dose'].str.replace('Dose 3', 'Third')
-
-# %%
-history = df[df['Nation']=='Northern Ireland'][['Publication Date','Dose','People']]
-ni.rename(columns={'Date':'Publication Date','Total':'People'}, inplace=True)
-all = history.merge(ni, on=['Publication Date','Dose'], how='outer', suffixes=('','_bot'))
-all['People'] = all['People'].fillna(all['People_bot'])
-all = all[['Publication Date','Dose','People']]
-
-# %%
-boosters = all[all['Dose']=='Booster'][['Publication Date','People']]
+boosters = all[all['Dose']=='Third/Booster'][['Publication Date','People']]
 boosters['Publication Date'] = pandas.to_datetime(boosters['Publication Date'])
 dose2s = all[all['Dose']=='Second'][['Publication Date','People']]
 dose2s['Publication Date'] = pandas.to_datetime(dose2s['Publication Date'])
@@ -38,12 +31,12 @@ dose2s['Booster Target Date 7M'] = pandas.to_datetime(dose2s['Publication Date']
 dose2s['Booster Target Date 8M'] = pandas.to_datetime(dose2s['Publication Date']) + pandas.to_timedelta(183+62, unit='d')
 boosters = boosters.merge(dose2s[['Booster Target Date 6M','People']], left_on='Publication Date', right_on='Booster Target Date 6M', how='left', suffixes=('', '_target'))
 boosters = boosters[['Publication Date','People','People_target']]
-boosters.rename(columns={'People':'Booster doses', 'People_target': 'Second doses 6 months earlier'}, inplace=True)
+boosters.rename(columns={'People':'Third/Booster doses', 'People_target': 'Second doses 6 months earlier'}, inplace=True)
 boosters = boosters.merge(dose2s[['Booster Target Date 7M','People']], left_on='Publication Date', right_on='Booster Target Date 7M', how='left', suffixes=('', '_target'))
-boosters = boosters[['Publication Date','Booster doses','Second doses 6 months earlier','People']]
+boosters = boosters[['Publication Date','Third/Booster doses','Second doses 6 months earlier','People']]
 boosters.rename(columns={'People': 'Second doses 7 months earlier'}, inplace=True)
 boosters = boosters.merge(dose2s[['Booster Target Date 8M','People']], left_on='Publication Date', right_on='Booster Target Date 8M', how='left', suffixes=('', '_target'))
-boosters = boosters[['Publication Date','Booster doses','Second doses 6 months earlier','Second doses 7 months earlier','People']]
+boosters = boosters[['Publication Date','Third/Booster doses','Second doses 6 months earlier','Second doses 7 months earlier','People']]
 boosters.rename(columns={'People': 'Second doses 8 months earlier'}, inplace=True)
 boosters = boosters.melt(id_vars='Publication Date', var_name='Metric', value_name='Doses')
 
@@ -66,7 +59,7 @@ plot_points_average_and_trend(
         datetime.datetime.today().strftime('%A %-d %B %Y'),
     ),
     [
-        'Dose 1/2 data from PHE dashboard/API, Dose 3/Booster collected from NI dashboard',
+        'Data from https://coronavirus.data.gov.uk',
         'https://twitter.com/ni_covid19_data'
     ]
 )
@@ -76,7 +69,7 @@ p = plot_points_average_and_trend(
     [
         {
             'points': None,
-            'line': boosters.set_index(['Publication Date','Metric'])['Doses'],
+            'line': boosters[boosters['Publication Date'] > '2021-10-01'].set_index(['Publication Date','Metric'])['Doses'],
             'colour': 'Metric',
             'date_col': 'Publication Date',
             'x_title': 'Date',
@@ -84,15 +77,13 @@ p = plot_points_average_and_trend(
             'scales': ['linear'],
             'height': 450,
             'width': 800,
-#            'colour_domain': ['Booster doses','Second doses 6 months earlier','Second doses 7 months earlier','Second doses 8 months earlier'],
-#            'colour_range': ['#ff0000','#2b7e9e','#52b4cf','#7eedff'],
         },
     ],
-    'NI COVID-19 booster vaccination progress vs second dose up to %s' %(
+    'NI COVID-19 booster/third vaccination progress vs second dose up to %s' %(
         datetime.datetime.today().strftime('%A %-d %B %Y'),
     ),
     [
-        'Dose 2 data from PHE dashboard/API, Booster data collected from NI dashboard',
+        'Data from https://coronavirus.data.gov.uk',
         'https://twitter.com/ni_covid19_data'
     ]
 )
--- notebooks/vacc_gap.py
+++ notebooks/vacc_gap.py
@@ -1,12 +1,14 @@
 # %%
 import pandas
 import altair
+import datetime
 
 # %%
-df = pandas.read_csv('https://api.coronavirus.data.gov.uk/v2/data?areaType=nation&metric=cumPeopleVaccinatedFirstDoseByPublishDate&metric=cumPeopleVaccinatedSecondDoseByPublishDate&format=csv')
+df = pandas.read_csv('https://api.coronavirus.data.gov.uk/v2/data?areaType=nation&metric=cumPeopleVaccinatedFirstDoseByPublishDate&metric=cumPeopleVaccinatedThirdInjectionByPublishDate&metric=cumPeopleVaccinatedSecondDoseByPublishDate&format=csv')
 df.rename(columns={
     'cumPeopleVaccinatedFirstDoseByPublishDate': 'First',
     'cumPeopleVaccinatedSecondDoseByPublishDate': 'Second',
+    'cumPeopleVaccinatedThirdInjectionByPublishDate': 'Third/Booster',
     'areaName': 'Nation',
     'date': 'Publication Date'
 }, inplace=True)
@@ -36,22 +38,156 @@ for nation in ['Northern Ireland','England','Wales','Scotland']:
     dfa['Publication Date'] = pandas.to_datetime(dfa['Publication Date'])
     dfa.set_index(['Publication Date'], inplace=True)
     dfa['Gap'] = (dfa.index - dfa['Second'].apply(latest_where_lte, col=dfa['First'])).dt.days
+    dfa['Gap2'] = (dfa.index - dfa['Third/Booster'].apply(latest_where_lte, col=dfa['Second'])).dt.days
     out = pandas.concat([out, dfa.reset_index()])
 
 
 # %%
-altair.Chart(out[~out['Gap'].isna()]).mark_line().encode(
-    x = 'Publication Date:T',
-    y = altair.Y('Gap:Q',title='First/Second Dose Gap (days)'),
-    color='Nation'
+latest = out[out['Publication Date'] == out['Publication Date'].max()]
+trend = altair.Chart(
+    out[(~out['Gap'].isna())]
+).mark_line().encode(
+    x = altair.X(
+        'Publication Date:T',
+        title='Publication Date of Second Dose'
+    ),
+    y = altair.Y(
+        'Gap:Q',
+        title='First to Second Dose Gap (days)',
+        scale=altair.Scale(zero=False)
+    ),
+    color=altair.Color(
+        'Nation',
+        legend=None,
+        type='nominal',
+        scale=altair.Scale(
+            domain=['England','Scotland','Wales','Northern Ireland'],
+            range=['grey','#005eb8','#D30731','#076543']
+        )
+    )
 )
+text = altair.Chart(latest).mark_text(
+    align='left',
+    baseline='middle',
+    dx=5
+).encode(
+    x = 'Publication Date',
+    y = altair.Y(
+        field='Gap',
+        type='quantitative',
+        aggregate='sum',
+    ),
+    color=altair.Color(
+        'Nation',
+        type='nominal',
+        scale=altair.Scale(
+            domain=['England','Scotland','Wales','Northern Ireland'],
+            range=['grey','#005eb8','#D30731','#076543']
+        )
+    ),
+    text = altair.Text('Nation')
+)
+p = altair.concat(
+    altair.layer(
+        trend,
+        text
+    ).properties(
+        height=450,
+        width=800,
+        title=altair.TitleParams(
+            ['Data from https://coronavirus.data.gov.uk',
+            'https://twitter.com/ni_covid19_data on %s' %datetime.datetime.now().strftime('%A %-d %B %Y')
+            ],
+            baseline='bottom',
+            orient='bottom',
+            anchor='end',
+            fontWeight='normal',
+            fontSize=10,
+            dy=10
+        ),
+    )
+).properties(
+    title=altair.TitleParams(
+        'Gap in days between total first and second dose vaccinations for UK nations',
+        anchor='middle',
+    )
+)
+p.save('uk-second-gap-%s.png'%(datetime.datetime.now().date().strftime('%Y-%m-%d')))
+p
 
 # %%
-altair.Chart(df[df['Publication Date'] > '2021-06-01']).mark_line().encode(
-    x = 'Publication Date:T',
-    y = 'People:Q',
-    color='Dose',
-    facet=altair.Facet('Nation', columns=2)
-).resolve_scale(
-    y='independent'
+trend = altair.Chart(
+    out[(~out['Gap2'].isna()) & (out['Publication Date'] > '2021-10-12')]
+).mark_line().encode(
+    x = altair.X(
+        'Publication Date:T',
+        title='Publication Date of Third/Booster'
+    ),
+    y = altair.Y(
+        'Gap2:Q',
+        title='Second to Third/Booster Dose Gap (days)',
+        scale=altair.Scale(zero=False)
+    ),
+    color=altair.Color(
+        'Nation',
+        legend=None,
+        type='nominal',
+        scale=altair.Scale(
+            domain=['England','Scotland','Wales','Northern Ireland'],
+            range=['grey','#005eb8','#D30731','#076543']
+        )
+    )
 )
+text = altair.Chart(latest).mark_text(
+    align='left',
+    baseline='middle',
+    dx=5
+).encode(
+    x = 'Publication Date',
+    y = altair.Y(
+        field='Gap2',
+        type='quantitative',
+        aggregate='sum',
+    ),
+    color=altair.Color(
+        'Nation',
+        type='nominal',
+        scale=altair.Scale(
+            domain=['England','Scotland','Wales','Northern Ireland'],
+            range=['grey','#005eb8','#D30731','#076543']
+        )
+    ),
+    text = altair.Text('Nation')
+)
+p = altair.concat(
+    altair.layer(
+        trend,
+        text
+    ).properties(
+        height=450,
+        width=800,
+        title=altair.TitleParams(
+            ['Data from https://coronavirus.data.gov.uk',
+            'https://twitter.com/ni_covid19_data on %s' %datetime.datetime.now().strftime('%A %-d %B %Y')
+            ],
+            baseline='bottom',
+            orient='bottom',
+            anchor='end',
+            fontWeight='normal',
+            fontSize=10,
+            dy=10
+        ),
+    )
+).properties(
+    title=altair.TitleParams(
+        'Gap in days between total second and third/booster dose vaccinations for UK nations',
+        anchor='middle',
+    )
+)
+p.save('uk-booster-gap-%s.png'%(datetime.datetime.now().date().strftime('%Y-%m-%d')))
+p
+
+
+# %%
+latest
+# %%
